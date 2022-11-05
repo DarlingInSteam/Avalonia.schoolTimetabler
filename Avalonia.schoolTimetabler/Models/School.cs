ed12e7c4931755762dccd911b37dc06a515ddbb5
DarlingInSteam <rombk2711@gmail.com>
2022-11-06 03:11:22 +0700

    add ConfirmUserSettings

---
+++ Avalonia.schoolTimetabler/Models/School.cs
@@ -0,0 +1,6 @@
+﻿namespace Avalonia.schoolTimetabler.Models;
+
+public class School
+{
+    
+}
---
+++ Avalonia.schoolTimetabler/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Avalonia.schoolTimetabler.Models;
+
+public class User
+{
+    public string Password;
+    public string UserName;
+
+    public User(string password, string userName)
+    {
+        Password = password;
+        UserName = userName;
+    }
+}
