c83bb454dca57a8584d902f0b8a4db9a00c5fc3e
darlingPc <rombk2711@gmail.com>
2022-12-17 07:35:28 +0700

    new design

--- Data/FakeDataBase/FDataBaseTimetable.cs
+++ Data/FakeDataBase/FDataBaseTimetable.cs
@@ -1,6 +1,28 @@
-﻿namespace Data.FakeDataBase;
+﻿using Data.Models;
+
+namespace Data.FakeDataBase;
 
 public class FDataBaseTimetable
 {
+    private readonly List<SchoolTimetable> _schoolTimetables;
+    public IEnumerable<SchoolTimetable> SchoolTimetables { get; set; }
+
+    private FDataBaseTimetable()
+    {
+        _schoolTimetables = new List<SchoolTimetable>();
+        SchoolTimetables = _schoolTimetables;
+    }
     
+    public void AddTimetable(SchoolTimetable schoolTimetable)
+    {
+        _schoolTimetables.Add(schoolTimetable);
+        SchoolTimetables = _schoolTimetables;
+    }
+
+    public static FDataBaseTimetable GetInstance()
+    {
+        return _instance ??= new FDataBaseTimetable();
+    }
+
+    private static FDataBaseTimetable? _instance = null;
 }
---
+++ SchoolTimetabler/ViewModels/CreateTimetableViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Data.FakeDataBase;
+using ReactiveUI;
+
+namespace SchoolTimetabler.ViewModels;
+
+public class CreateTimetableViewModel : ViewModelBase, IRoutableViewModel, IScreen
+{
+    public CreateTimetableViewModel(IScreen hostScreen, FDataBaseTimetable storageTimetable)
+    {
+        HostScreen = hostScreen;
+    }
+
+    public string? UrlPathSegment { get; }
+    public IScreen HostScreen { get; }
+    public RoutingState Router { get; }
+}
