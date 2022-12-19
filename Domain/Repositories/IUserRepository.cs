e7909067affbe7f67096a355f395c63b21082d1f
darlingPc <rombk2711@gmail.com>
2022-12-20 01:58:58 +0700

    extra save

--- Domain/Repositories/IUserRepository.cs
+++ Domain/Repositories/IUserRepository.cs
@@ -1,1 +1,1 @@
-namespace Domain.Repositories;public interface IUserRepository<T>{    void Delete();    void Add(T newEntity);    T GetUserInfo();}
+namespace Domain.Repositories;public interface IUserRepository<T>{    void Delete();    void Add(T newEntity);    T Read();}
